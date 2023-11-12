ca5a7f6f1ac026fa289d908c3cadf41e8cee633c
john ossai <johnossai20@gmail.com>
2023-11-12 06:26:17 -0800

    worked on an error

--- frontend/src/pages/search.js
+++ frontend/src/pages/search.js
@@ -1,8 +1,11 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { foodData } from "@/foodData";
+import { useParams } from "next/navigation";
 
 const search = () => {
+  const params = useParams();
+  console.log(params);
   const router = useRouter();
 
   console.log(router);
@@ -11,13 +14,13 @@ const search = () => {
 
   useEffect(() => {
     const searchQ = router.query.foodName;
+
     const result = foodData.filter((foodItem) => {
       const foodsItem = foodItem.name.toLowerCase();
       return foodsItem.includes(searchQ);
     });
-
     setSearchResult(result);
-  }, []);
+  }, [router.query.foodName]);
 
   return (
     <div>
