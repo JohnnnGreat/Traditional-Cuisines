df6bc081074a4049324a7a8ddfe714c8acb29ea3
john ossai <johnossai20@gmail.com>
2023-11-18 12:49:38 -0800

    started working on the individual pages

--- frontend/src/pages/food.js
+++ frontend/src/pages/food.js
@@ -63,7 +63,7 @@ const FoodItem = () => {
           <img
             src="https://images.unsplash.com/photo-1540100716001-4b432820e37f?q=80&w=1512&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Alt"
-            className="food_image"
+            className="food_image-main"
           />
           <div className="text_details">
             {" "}
