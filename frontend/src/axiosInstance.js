4351d39e7d31eb396d86e7e238912fb1850608aa
John Ossai <johnossai20@gmail.com>
2024-01-02 02:05:32 -0800

    corrected responsive irresponsiveness

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "ttps://soulfood.onrender.com/" });
+const API = axios.create({ baseURL: "https://soulfood.onrender.com/" });
 
 //Production Url : https://soulfood.onrender.com
 export default API;
