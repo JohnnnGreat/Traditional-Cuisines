4f1b3e7a19b928ecb93e7d11282cc954d92af030
John Ossai <johnossai20@gmail.com>
2024-01-03 07:42:17 -0800

    worked on the login error

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -150,7 +150,7 @@ const GenerateCode = async (req, res) => {
       });
 
       const mailData = {
-        from: "JOHN OSSAI <johnossai20@gmail.com>",
+        from: "VERIFY EMAIL ADDRESS <johnossai20@gmail.com>",
         to: `<${email}>`,
         subject: `Verify Email Address`,
         html: `Click the link to verify http://localhost:3000/profile?code=${stringRepresentation}`,
